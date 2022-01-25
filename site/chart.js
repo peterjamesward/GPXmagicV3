6874a586819161c6478f6e99a54c2f4480355f37
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2022-01-25 14:14:35 +0000

    Stable chart.
    (cherry picked from commit 5376bf7a7e2c56d6a1ab443ec5bb26e36258d887)

--- site/chart.js
+++ site/chart.js
@@ -7,43 +7,67 @@ const data = [{"distance":0,"altitude":12.467755997081404,"gradient":0.189787625
 {"distance":30.644305269902187,"altitude":12.52591510955149,"gradient":2.18978822408369428},
 {"distance":32.06982509852133,"altitude":12.528620578318186,"gradient":0.1897884712266169},
 {"distance":33.495343070826664,"altitude":12.531326047084885,"gradient":0.18978877860236976},
-{"distance":34.92085873440838,"altitude":12.534031515851582,"gradient":1.18978914581778442},
+{"distance":34.92085873440838,"altitude":12.534031515851582,"gradient":0.18978914581778442},
 {"distance":36.346371639817875,"altitude":12.536736984618281,"gradient":0.18978957230930815},
 {"distance":37.77188134184183,"altitude":12.53944245338498,"gradient":0.18979005728712342},
 {"distance":39.197387401205226,"altitude":12.542147922151678,"gradient":0.18979060049617877}]
 
+const xExtent = fc.extentLinear()
+  .accessors([d => d.distance])
+  .include([0, 40]);
 
-const chart = Plot.plot({
-  x: {
-    grid: true
-  },
-  y: {
-    grid: true
-  },
-  color: {
-    scheme: "turbo",
-    type: "linear",
-    domain: [-1, 5],
-    range: [0, 1]
-  },
-  marks: [
-    Plot.areaY(data,
-        {x: "distance",
-        y1: 12.4,
-        y2: d => d.altitude,
-        fill: "gradient" //"#EEEEEE"
-        }),
-    Plot.lineY(data,
-        {x: "distance",
-        y: "altitude",
-        stroke: "blue"
-        }),
-    Plot.dot(data, {
-      x: "distance",      // feature for the x channel
-      y: "altitude",     // feature for the y channel
-        fill: "gradient" //"#EEEEEE"
-    })
-  ]
-})
+const yExtent = fc.extentLinear()
+  .accessors([d => d.altitude])
+  .include([12.2, 12.7]);
 
-document.body.appendChild(chart);
+const xAxis = fc.axisBottom(xExtent)
+  .tickArguments([5])
+  .tickCenterLabel(true);
+
+const yAxis = fc.axisBottom(yExtent)
+  .tickArguments([5])
+  .tickCenterLabel(true);
+
+// gridlines (from d3fc-annotation)
+// n.b. the gridlines are rendered using SVG
+var gridlines = fc.annotationSvgGridline();
+
+var line = fc.seriesCanvasLine()
+    .crossValue(d => d.distance)
+    .mainValue(d => d.altitude)
+    .decorate((context, datum, index) => {
+        context.fillStyle = '#4444ff';
+      });
+
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var area = fc.seriesCanvasArea()
+    .crossValue(d => d.distance)
+    .mainValue(d => d.altitude)
+    .decorate((context, datum, index) => {
+        context.fillStyle = '#eeeeee';
+    });
+
+// combine into a single series
+var multi = fc.seriesCanvasMulti()
+  .series([area, line]);
+
+// the Cartesian component, which uses d3fc-element for layout
+// of the standard features of a chart (axes, labels, plot area)
+var chart = fc.chartCartesian(
+    d3.scaleLinear(),
+    d3.scaleLinear()
+  )
+  .xLabel('Distance')
+  .yLabel('Altitude')
+  .yDomain(yExtent(data))
+  .xDomain(xExtent(data))
+  .yOrient('left')
+  .svgPlotArea(gridlines)
+  .canvasPlotArea(multi);
+
+// render
+d3.select('#chart')
+  .datum(data)
+  .call(chart);
+
