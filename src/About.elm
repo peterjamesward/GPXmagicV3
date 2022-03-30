4d5ade14bc09bcc59c620e3ca4adb11685e1760f
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2022-03-30 13:22:40 +0100

    3.1.2 prep

--- src/About.elm
+++ src/About.elm
@@ -3,30 +3,15 @@ module About exposing (..)
 
 aboutText =
     """
-# GPXmagic v3.1.1
+# GPXmagic v3.1.2
 
 (7f551b2e)
 
 ## In this release ...
 
-* When you're building a route in _Route maker_ and you want to "double back" along the same Road,
-you can now click on the Place and add a loop back to the same Place. Use the Radius slider in the
-Route maker tool to adjust the radius. You can then add this loop to your route; be sure to check
-the direction you use!
+* Terrain is back, same as v2. Just a bit of fun.
 
-* _Route maker_ will warn if there are many Places (more than 10% of trackpoints). Remember, it's
-intended for use with _planned_ routes, not _recorded_ rides. (Thanks David, again.)
-
-* The joins at Places are better but I advise you to always check and smooth them after you have
-created the new route.
-
-* Some bug fixes.
-
-## Coming up, not necessarily in order ...
-
-1. Terrain
-2. Land use (what?)
-3. French (quoi?)
+* Fixed bug affecting height of result list in Bend and Gradient problems.
 
 ## Legal guff
 
