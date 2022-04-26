daa099e20afb4b499d4d3ff99875938b03063b66
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2022-04-26 16:13:37 +0100

    Closed source. 3.2.9 prep.

--- src/About.elm
+++ src/About.elm
@@ -3,13 +3,13 @@ module About exposing (..)
 
 aboutText =
     """
-# GPXmagic v3.2.8
+# GPXmagic v3.2.9
 
 (3a60117c)
 
 ## In this release ...
 
-* Language editor.
+* Changes to license terms.
 
 ## Acknowledgements
 
@@ -19,20 +19,20 @@ aboutText =
 
 ## Legal guff
 
-> peterjamesward/GPXmagicV3 is licensed under the
-> Creative Commons Zero v1.0 Universal license
-
-Source code available: https://github.com/peterjamesward/GPXmagicV3
-
 Compatible with Strava, for the purpose of loading route and segment data.
 
-Map component provided by MapBox.com
+GPXmagicV3 is currently not open-source but source code is available on request.
+
+Contains numerous libraries under various licence terms, all of which are available in source
+form via https://package.elm-lang.org.
+
+Map component provided by MapBox.com.
 
 Land use data courtesy of Open Street Map via the Overpass API.
 
 Your IP address may be logged for the purpose of aggregate usage recording; no personal details are stored.
 
-No cookies are used, though many chocolate digestives were consumed whilst writing.
+Cookie policy is "use no cookies". This may not apply to third-party components.
 
 
     """
