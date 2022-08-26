d90861dd17e5ec1a9f4ddf03f2dd943f28b37d78
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2022-08-26 09:25:41 +0100

    Prep 3.4.3 new option in split and join.

--- src/About.elm
+++ src/About.elm
@@ -3,7 +3,7 @@ module About exposing (..)
 
 aboutText =
     """
-# GPXmagic v3.4.2 (a156c880)
+# GPXmagic v3.4.2 (6b96e98b)
 
 **GPXmagic V3 works (by far) best with Chrome (and some derivatives)**
 
