6dfc4d008abd68dcdb342fd866e7ccd65ebd5312
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2023-02-15 20:27:35 +0000

    Prep 3.9.8

--- src/About.elm
+++ src/About.elm
@@ -3,7 +3,7 @@ module About exposing (aboutText)
 
 aboutText =
     """
-# GPXmagic v3.9.8 (880efbcd)
+# GPXmagic v3.9.8 (2a43f6c7)
 
 **GPXmagic V3 works best with Chrome (and some derivatives)**
 
