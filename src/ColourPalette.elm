546052314f253a5ef30e4031be44818e138986d0
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2023-05-15 08:42:38 +0100

    Tweak gradient palette.

--- src/ColourPalette.elm
+++ src/ColourPalette.elm
@@ -7,11 +7,16 @@ import FlatColors.FlatUIPalette exposing (..)
 
 gradientColourPastel : Float -> Color.Color
 gradientColourPastel slope =
-    Color.hsl (gradientHue slope) 0.6 0.7
+    Color.hsl (gradientHue slope) 0.8 0.6
 
 
-gradientHue : Float -> Float
-gradientHue slope =
+gradientColourVivid : Float -> Color.Color
+gradientColourVivid slope =
+    Color.hsl (gradientHue slope) 1.0 0.4
+
+
+gradientHueOld : Float -> Float
+gradientHueOld slope =
     let
         x =
             (clamp -20.0 20.0 slope + 20.0) / 40.0
@@ -23,6 +28,37 @@ gradientHue slope =
             (Color.toHsla <| Color.rgb255 143 87 255).hue
     in
     x * steepestAscentHue + (1.0 - x) * steepestDescentHue
+
+
+gradientHue : Float -> Float
+gradientHue slope =
+    let
+        hueOf col =
+            let
+                { hue, saturation, lightness, alpha } =
+                    Color.toHsla col
+            in
+            hue
+    in
+    -- Closer to "standard" colouring.
+    if slope < 0 then
+        gradientHue slope
+
+    else if slope <= 6.0 then
+        interpolate (slope / 6.0) (hueOf Color.lightGreen) (hueOf Color.yellow)
+
+    else if slope <= 9.0 then
+        interpolate ((slope - 6.0) / 3.0) (hueOf Color.yellow) (hueOf Color.orange)
+
+    else if slope <= 12.0 then
+        interpolate ((slope - 9.0) / 3.0) (hueOf Color.orange) (hueOf Color.red)
+
+    else
+        interpolate ((clamp 12 30 slope - 12.0) / 18.0) (hueOf Color.red) (hueOf Color.black)
+
+
+interpolate x a b =
+    x * a + (1.0 - x) * b
 
 
 stravaOrange =
